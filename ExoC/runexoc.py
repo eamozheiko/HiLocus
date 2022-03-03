bcf4eba48ea0012268382f122a24189c17669473
eamozheiko <eamozheiko@gmail.com>
2022-03-03 22:40:17 +0700

    fix3

--- ExoC/runexoc.py
+++ ExoC/runexoc.py
@@ -19,20 +19,10 @@ from ExoC.corelib import *
 
 def statrun(args):
     opts = opt_validate_stat(args)
-    #command = "Rscript %s %s %s %s"%(args.sam, args.name, args.outdir)
-    #print(command)
-    #run_cmd(command)
-    #print(os.path.abspath('.'))
-    #print(opts)
-    print(os.path.abspath('../ExoC/sam_to_valid_pairs.sh') + " " + opts.sam + " " + opts.outdir + " " + opts.name)
-    subprocess.call(os.path.abspath('../ExoC/sam_to_valid_pairs.sh') + " " + opts.sam + " " + opts.outdir + " " + opts.name, shell=True)
-    #print(os.path.abspath('../ExoC/t.sh') + " " + opts.sam + " " + opts.name + " " + opts.outdir)
-    #subprocess.call(os.path.abspath('../ExoC/t.sh') + " " + opts.sam + " " + opts.name + " " + opts.outdir, shell=True)
     
-    #command = "Rscript %s %s"%(os.path.abspath('../ExoC/t.R'), os.path.abspath('..'))
-    #run_cmd(command)
-    #print(command)
-    # Rscript
+    path('../ExoC/sam_to_valid_pairs.sh') + " " + opts.sam + " " + opts.outdir + " " + opts.name)
+    subprocess.call(os.path.abspath('../ExoC/sam_to_valid_pairs.sh') + " " + opts.sam + " " + opts.outdir + " " + opts.name, shell=True)
+
     
     Info("Done! Find your stat results from %s" %(args.outdir))
     return
@@ -40,12 +30,7 @@ def statrun(args):
 def cnvrun(args):
     opts = opt_validate_cnv(args)
     
-    ## calculate cnv pattern
-    #command = "Rscript cnv_pattern.R %s %s %s %s"%(os.path.abspath('../ExoC/t.R'), outputsubdir,matrixfile,tempbpoutputfile)
-    #print(command)
-    #run_cmd(command)
-    
-    #Info("Done! Find your CNV results from %s" %(args.outdir))
+
     Info("CNV module missing")
     return
 
@@ -58,16 +43,11 @@ def transrun(args):
     print(command)
     run_cmd(command)
     
-
+    ## intra
     command = "Rscript %s %s %s %s %s %s %s %s"%(os.path.abspath('../ExoC/intra_trans.R'), os.path.abspath('..'), opts.outdir, opts.case, opts.control, opts.binsize, opts.name, opts.thr_frame)
     print(command)
     run_cmd(command)
-    #run_cmd(command)
-    
-    ## intra
-    #command = "Rscript inter_trans.R %s %s %s %s"%("../ExoC/intra_trans.R", opts.case, opts.control, opts.binsize, opts.name, opts.outdir)
-    #print(command)
-    #run_cmd(command)
+
     
     Info("Done! Find your translocation results from %s"%(opts.outdir))
     return
